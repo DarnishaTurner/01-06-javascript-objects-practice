cc0eef1b295d673a5ffbcf86b8d28c1999a72d13
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-05 21:40:10 -0500

    Darnishas Version

---
+++ objects.js
@@ -0,0 +1,8 @@
+// 1. Create an object named 'person' with properties 'firstName' and 'lastName'.
+let person = {
+    firstName: 'Alice',
+    lastName: 'Smith'
+};
+
+// 2. Log the full name of the person to the console by accessing the object's properties.
+console.log(`Full name: ${person.firstName} ${person.lastName}`);
